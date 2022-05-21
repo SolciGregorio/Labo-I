b95043b2719e8f4f21438de2f061a580a5cc37e8
SolciGregorio <83075808+SolciGregorio@users.noreply.github.com>
2022-05-21 15:30:41 -0300

    Update Electric Power Company.cpp
    se arreglo switch

--- Electric Power Company.cpp
+++ Electric Power Company.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-int main() {
-
+int main()
+{
     int nC;
     int lC;
     int kV;
 
-    int t1 = 0;
-    int t2 = 0;
-    int t3 = 0;
+    int t1 =0;
+    int t2 =0;
+    int t3 =0;
     int i =0;
-    int max1=0;
-    int max2=0;
-    int max3=0;
-    int N;
+  //  int max1=0;
+  // int max2=0;
+  // int max3=0;
+  // int N;
 
     cout << "Ingrese los datos nro de cliente: ";
     cin >> nC;
@@ -23,67 +23,77 @@ int main() {
     cout << "Ingrese los datos kilovatios: ";
     cin >> kV;
 
-    while(N!=0){
-        switch (lC){
-
-            case 1: if (kV<100){
-
-                i=(kV+100)*10;
-            }
-            else if (kV<200){
-
+    switch (lC)
+    {
+    case 1:
+        if(kV<100)
+        {
+            i=(kV+100)*10;
+        }
+        else
+        {
+            if (kV<200)
+            {
                 i=(kV+100)*12;
             }
-            else {
-
-                i=(kV+100)*15;
-            }
-
-            t1=t1+=i;
-        break;
-
-            case 2: if(kV<100){
-
-                i=(kV+100)*10;
-            }
-            else if (kV<200){
-
-                i=(kV+100)*12;
-            }
-            else{
-                i=(kV+100)*15;
-            }
-
-            t2=t2+=i;
-        break;
-
-            case 3: if (kV<100){
-
-                i=(kV+100)*10;
-            }
-            else if (kV<200){
-
-                i=(kV+100)*12;
-            }
-            else {
+            else
+            {
                 i=(kV+100)*15;
             }
         }
-            t3=t3+=i;
+        t1+=i;
+
         break;
 
+    case 2:
+        if(kV<100)
+        {
+            i=(kV+100)*10;
+        }
+        else
+        {
+            if (kV<200)
+            {
+                i=(kV+100)*12;
+            }
+            else
+            {
+                i=(kV+100)*15;
+            }
+        }
+        t2+=i;
 
-    cout << "Ingrese los datos nro de cliente: ";
-    cin >> nC;
-    cout << "Ingrese los datos localidad: ";
-    cin >> lC;
-    cout << "Ingrese los datos kilovatios: ";
-    cin >> kV;
+        break;
+
+    case 3:
+        if (kV<100)
+        {
+            i=(kV+100)*10;
+        }
+        else
+        {
+
+            if (kV<200)
+            {
+                i=(kV+100)*12;
+            }
+            else
+            {
+                i=(kV+100)*15;
+            }
+        }
+        t3+=i;
+
+        break;
+
     }
-    cout << "El monto total de la localidad 3 es: "<<t3;
-    cout << "El monto total de la localidad 2 es: "<<t2;
-    cout << "El monto total de la localidad 1 es: "<<t1;
+
+    cout << "El monto total de la localidad 3 es: "<<t3 << endl;
+
+    cout << "El monto total de la localidad 2 es: "<<t2 <<endl;
+
+    cout << "El monto total de la localidad 1 es: "<<t1 <<endl;
 
     system("pause");
-    return 1;
+    return 0;
 }
