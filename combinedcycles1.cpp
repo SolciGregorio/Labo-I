f897e84d72217f96ad4d4258b4d96b4fbab0f139
SolciGregorio <83075808+SolciGregorio@users.noreply.github.com>
2022-05-22 13:42:00 -0300

    Update combinedcycles1.cpp

--- combinedcycles1.cpp
+++ combinedcycles1.cpp
@@ -1,1 +1,26 @@
-# laboratorio1
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    int i,months,numberOfStudents,courseNumber, studentAccumulator = 0;
+
+    for (i=1; i<=12; i++){
+        for (i=1; i<=7; i++){
+            cout<<"ingresar mes:  " << endl;
+            cin>> months;
+
+            cout<<"ingresar cantidad  de alumnos:  " << endl;
+            cin>>numberOfStudents;
+
+            cout<<"ingresar numero de curso:  " << endl;
+            cin>>courseNumber;
+
+            studentAccumulator= studentAccumulator+numberOfStudents;
+        }
+        cout<<"la cantidad mes : " << studentAccumulator;
+        cout<<"la cantidad : " << studentAccumulator;
+      }
+    system("pause");
+    return 0;
+}
