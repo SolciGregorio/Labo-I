135fc31a68b8cef136d4f08dcc0b2da8335f2246
Solci Gregorio <83075808+SolciGregorio@users.noreply.github.com>
2023-03-30 20:57:11 -0300

    Update Guia 1 ej 2.cpp

--- Guia 1 ej 2.cpp
+++ Guia 1 ej 2.cpp
@@ -1,5 +1,9 @@
-/* Hacer un programa para ingresar por teclado la cantidad de horas trabajadas por un operario y
-el valor que se le paga por hora trabajada y listar por pantalla el sueldo que le corresponda. */
+//Nombre:Maria Sol Gregorio
+//TP N° :1
+// Ej N°:2
+
+/* Hacer un programa que solicite por teclado que se ingresen dos números y luego guardarlos en dos variables distintas.
+ A continuación se deben intercambiar mutuamente los valores en ambas variables y mostrarlos por pantalla.*/
 
 #include <iostream>
 #include <cstdlib>
@@ -7,23 +11,21 @@ using namespace std;
 
 int main()
 {
- int valorA;
- int valorB;
- int auxiliar;
-// int resultado;
- 
- 
-cout << "ingrese primer valor:  ";
-cin >> valorA;
- cout << "ingrese segundo valor: ";
-cin >> valorB;
-auxiliar = valorA;
-valorA = valorB;
-valorB = auxiliar;
-//sueldoEmpleado = cantidadHoras * sueldoEmpleado;
-cout<< "el segundo valor es: "<<valorA<<endl;
-cout<< "el primer valor:: "<<valorB<<endl;
- 
+    int valorA;
+    int valorB;
+    int auxiliar;
+
+
+    cout << "ingrese primer valor:  ";
+    cin >> valorA;
+    cout << "ingrese segundo valor: ";
+    cin >> valorB;
+    auxiliar = valorA;
+    valorA = valorB;
+    valorB = auxiliar;
+    cout<< "el segundo valor es: "<<valorA<<endl;
+    cout<< "el primer valor:: "<<valorB<<endl;
+
 return 0;
 
 }
