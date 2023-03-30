43d6930e49bfa879f0e5180e3b483470726a2db2
Solci Gregorio <83075808+SolciGregorio@users.noreply.github.com>
2023-03-30 20:53:05 -0300

    Update Guia 1 ej 1.cpp

--- Guia 1 ej 1.cpp
+++ Guia 1 ej 1.cpp
@@ -1,5 +1,10 @@
-/* Hacer un programa para ingresar por teclado la cantidad de horas trabajadas por un operario y
-el valor que se le paga por hora trabajada y listar por pantalla el sueldo que le corresponda. */
+//Nombre:Sol Gregorio
+//TP N° :1
+// Ej N°:1
+
+/*Comentarios:/* Hacer un programa para ingresar por teclado la cantidad de horas
+ trabajadas por un operario y el valor que se le paga por hora trabajada y listar por pantalla el sueldo que le corresponda./*/
+
 #include <iostream>
 #include <cstdlib>
 using namespace std;
@@ -9,15 +14,15 @@ int main()
  int cantidadHoras;
  int valorHora;
  int sueldoEmpleado;
- 
- 
+
+
 cout << "ingrese cantidad de horas trabajadas:  ";
 cin >> cantidadHoras;
- cout << "ingrese el valor de la hora: ";
+cout << "ingrese el valor de la hora: ";
 cin >> sueldoEmpleado;
 sueldoEmpleado = cantidadHoras * sueldoEmpleado;
 cout<< "el total a pagar es: "<<sueldoEmpleado;
- 
+
 return 0;
 
 }
