8d92e590d669f78c454053188307b4bcb73453ed
Solci Gregorio <83075808+SolciGregorio@users.noreply.github.com>
2023-03-30 22:16:53 -0300

    Update and rename Guia 1 ej 6.cpp to TP1 EJ 6.cpp

---
+++ TP1 EJ 6.cpp
@@ -0,0 +1,46 @@
+//Nombre:Maria Sol Gregorio
+//TP N° :1
+//Ej N°:6
+
+/*Hacer un programa para que un comercio ingrese por teclado la recaudación en pesos para cada una de las cuatro semanas del mes.
+El programa debe listar la recaudación promedio por semana y el porcentaje de recaudación por semana.*/
+
+
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int semanaUno,semanaDos,semanaTres,semanaCuatro,totalRecaudacion;
+    float RecPromedio,porcSemanaUno,porcSemanaDos,porSemanaTres,porSemanaCuatro;
+
+    cout << "Cantidad de RECAUDACION SEMANA 'UNO': "<< endl;
+    cin >>semanaUno;
+    cout << "Cantidad de RECAUDACION SEMANA 'DOS': "<< endl;
+    cin >>semanaDos;
+    cout << "Cantidad de RECAUDACION SEMANA 'TRES': "<< endl;
+    cin >>semanaTres;
+    cout << "Cantidad de RECAUDACION SEMANA 'CUATRO': "<< endl;
+    cin >>semanaCuatro;
+    totalRecaudacion = semanaUno + semanaDos+semanaTres+semanaCuatro;
+    porcSemanaUno = (float)semanaUno * 100 /(float) totalRecaudacion;
+    porcSemanaDos = (float)semanaDos * 100 / (float)totalRecaudacion;
+    porSemanaTres = (float)semanaTres * 100 / (float)totalRecaudacion;
+    porSemanaCuatro = (float)semanaCuatro * 100 / (float)totalRecaudacion;
+    RecPromedio = totalRecaudacion/4;
+
+    cout << "Porcentaje de RECAUDACION : "<< porcSemanaUno <<"%"<< endl;
+    cout << "Porcentaje de RECAUDACION : "<< porcSemanaDos <<"%"<< endl;
+    cout << "Porcentaje de RECAUDACION : "<< porSemanaTres <<"%"<< endl;
+    cout << "Porcentaje de RECAUDACION : "<< porSemanaCuatro <<"%"<< endl;
+    cout << endl;
+    cout << "EL PROMEDIO DE RECAUDACION : "<< RecPromedio << endl;45
+
+    cout <<"----------------------------------------------------------------  ";
+
+
+    return 0;
+
+}
