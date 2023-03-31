440eb81f4a92d31311abec90a762f273677010e7
Solci Gregorio <83075808+SolciGregorio@users.noreply.github.com>
2023-03-30 21:37:33 -0300

    Update and rename Guia 1 ej 5.cpp to TP1 EJ 5.cpp

--- TP1 EJ 5.cpp
+++ TP1 EJ 5.cpp
@@ -1,35 +1,39 @@
-/* Hacer un programa para ingresar por teclado la cantidad de asientos disponibles en un avión y la cantidad 
-de pasajes ocupados y luego calcular e informar el porcentaje de ocupación y el porcentaje de no ocupación del mismo.. */
+//Nombre:Maria Sol Gregorio
+//TP N° :1
+//Ej N°:5
+
+/* Un comercio vende tres marcas de alfajores distintas A, B y C. Hacer un programa para ingresar por teclado la cantidad de alfajores vendidos de cada una de las tres marcas
+y luego se informe el porcentaje de ventas para cada una de ellas.*/
+
+
 
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-int main() {
-    int alfajorA,alfajorB,alfajorC,totalGeneral;
+
+int main()
+{
+
+int alfajorA,alfajorB,alfajorC,totalGeneral;
     float porcAlfajorA,porcAlfajorB,porcAlfajorC;
     cout << "Cantidad de Alfajores marca 'A': ";
     cin >>alfajorA;
-    cout << endl;
     cout << "Cantidad de Alfajores marca 'B': ";
     cin >>alfajorB;
-    cout << endl;
     cout << "Cantidad de Alfajores marca 'C': ";
     cin >>alfajorC;
-    cout << endl;
     totalGeneral = alfajorA + alfajorB+alfajorC;
     porcAlfajorA = (float)alfajorA * 100 /(float) totalGeneral;
     porcAlfajorB = (float)alfajorB * 100 / (float)totalGeneral;
     porcAlfajorC = (float)alfajorC * 100 / (float)totalGeneral;
-    
+
 
     cout <<endl;
-    cout << "Porcentaje de Alfajores marca 'A': "<< porcAlfajorA <<"%"<< endl;;
-	cout << endl;
-    cout << "Porcentaje de Alfajores marca 'B': "<< porcAlfajorB <<"%"<< endl;;
-    cout << endl; 
+    cout << "Porcentaje de Alfajores marca 'A': "<< porcAlfajorA <<"%"<< endl;
+    cout << "Porcentaje de Alfajores marca 'B': "<< porcAlfajorB <<"%"<< endl;
     cout << "Porcentaje de Alfajores marca 'C': "<< porcAlfajorC <<"%"<< endl;;
     cout <<"----------------------------------------------------------------  ";
 
-    system("pause");
     return 0;
+
 }
